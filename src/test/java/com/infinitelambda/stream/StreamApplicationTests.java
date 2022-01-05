9169cd369c630850a2f09862c8aae741974fe357
Andrew Chan <andrew.koonkit.chan@gmail.com>
2022-01-05 15:44:40 +0000

    first commit

---
+++ src/test/java/com/infinitelambda/stream/StreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infinitelambda.stream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
